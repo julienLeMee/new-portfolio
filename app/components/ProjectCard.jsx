0b6458a2f05ec6183eecffdf409feaaee1d6b486
Julien Le Mee <julemee@gmail.com>
2024-10-18 19:51:28 -0400

    nouvelle cartes projects

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -12,31 +12,26 @@ export default function ProjectCard({
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="inline-flex flex-col dark:bg-white bg-[--color-dark-bg] w-[calc(100svw-48px)] max-w-[560px] rounded-3xl p-3 pb-5 md:w-[60lvw] md:max-w-[632px] md:rounded-[32px] md:p-6 lg:w-[35lvw] transition-all hover:bg-[--color-pink] dark:hover:bg-[--color-pink]"
+      className="h-[700px] inline-flex flex-1 hover:flex-[4] flex-col bg-[--color-dark-bg] transition-all duration-300 hover:bg-[--color-pink] dark:hover:bg-[--color-pink] relative"
+      style={{
+        backgroundImage: `url(${imageSrc})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
     >
-      <div className="relative mb-4 h-[232px] w-full overflow-hidden rounded-2xl md:rounded-[20px]">
-        <img
-          src={imageSrc}
-          alt={title}
-          className="absolute inset-[-1] size-full object-cover"
-        />
+      <div className="relative mb-4 h-full w-full overflow-hidden">
       </div>
-      <h3 className="mb-6 text-2xl font-semibold leading-[1.1]md:text-[32px] text-white dark:text-black">
+      <h3 className="mb-6 text-2xl font-semibold leading-[1.1] md:text-[32px] text-white dark:text-black relative z-10">
         {title}
       </h3>
-      <p className="mb-6 flex-1 text-lg font-normal leading-[1.2]md:text-2xl md:leading-[1.1] text-white dark:text-black">
+      <p className="hidden mb-6 flex-1 text-lg font-normal leading-[1.2] md:text-2xl md:leading-[1.1] text-white dark:text-black relative z-10">
         {description}
       </p>
-      <div className="flex flex-wrap gap-1 mb-3">
+      <div className="flex flex-wrap gap-1 mb-3 relative z-10">
         {technologies.map((tech, index) => (
-          <ShinyBadge
-            key={index}
-            text={tech.name}
-            logo={tech.logo}
-          />
+          <ShinyBadge key={index} text={tech.name} logo={tech.logo} />
         ))}
       </div>
-      {/* <span className="text-blue-500 hover:underline">View Project</span> */}
     </a>
   );
 }
--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,10 +1,122 @@
 import ProjectCard from "./ProjectCard";
-import projects from "@/data/projectsData";
 
 export default function Projects() {
+  const projects = [
+    // YUM LIST
+    {
+      imageSrc: "/projects/yum-list.jpeg",
+      title: "Yum list",
+      description:
+        "Progressive Web App. Organize your favorite restaurants, whether they are tested or to discover. Connect with your friends and share your favorite addresses.",
+      technologies: [
+        { name: "Ruby on rails", logo: "/logos/rails.svg" },
+        { name: "Javascript", logo: "/logos/javascript.svg" },
+        { name: "Tailwind", logo: "/logos/tailwindcss.svg" },
+        { name: "Google Map API", logo: "/logos/google-map.svg" },
+      ],
+      keyFeatures: [
+        {
+          text: "PWA for seamless mobile experience",
+          icon: "/icons/mobile-icon.svg",
+        },
+        {
+          text: "Integration with Google Maps for restaurant locations",
+          icon: "/icons/mobile-icon.svg",
+        },
+        {
+          text: "Social sharing features for favorite spots",
+          icon: "/icons/mobile-icon.svg",
+        },
+      ],
+      link: "https://link-to-project-one.com",
+      github: "https://github.com/julienLeMee/yum-list",
+    },
+    // NIKE CLONE
+    {
+      imageSrc: "/projects/nike.jpg",
+      title: "Nike Clone",
+      description: "A responsive clone of Nike's e-commerce platform.",
+      technologies: [
+        { name: "Next JS", logo: "/logos/nextjs.svg" },
+        { name: "Javascript", logo: "/logos/javascript.svg" },
+        { name: "Tailwind", logo: "/logos/tailwindcss.svg" },
+      ],
+      keyFeatures: [
+        {
+          text: "Product filtering and dynamic cart functionality",
+          icon: "/icons/filtering.svg",
+        },
+        {
+          text: "Fully responsive across all devices",
+          icon: "/icons/responsive.svg",
+        },
+        { text: "Optimized for fast load times", icon: "/icons/fast-load.svg" },
+      ],
+      link: "https://link-to-project-one.com",
+      github: "https://github.com/julienLeMee/yum-list",
+    },
+    // DISNEY CLONE
+    {
+      imageSrc: "/projects/disney.jpeg",
+      title: "Disney Clone",
+      description: "A clone of Disney's streaming service interface.",
+      technologies: [
+        { name: "Next JS", logo: "/logos/nextjs.svg" },
+        { name: "Javascript", logo: "/logos/javascript.svg" },
+        { name: "Tailwind", logo: "/logos/tailwindcss.svg" },
+      ],
+      keyFeatures: [
+        {
+          text: "Interactive UI with smooth transitions",
+          icon: "/icons/ui.svg",
+        },
+        {
+          text: "Streaming content simulation with API integration",
+          icon: "/icons/api.svg",
+        },
+        {
+          text: "User authentication and secure session handling",
+          icon: "/icons/authentication.svg",
+        },
+      ],
+      link: "https://link-to-project-one.com",
+      github: "https://github.com/julienLeMee/yum-list",
+    },
+    // LA FERME DE NOVELLE
+    {
+      imageSrc: "/projects/la-ferme-de-novelle.jpeg",
+      title: "La ferme de Novelle",
+      description: "A website for a local farm featuring a map and store.",
+      technologies: [
+        { name: "Next JS", logo: "/logos/nextjs.svg" },
+        { name: "MapBox API", logo: "/logos/mapbox.svg" },
+        { name: "Tailwind", logo: "/logos/tailwindcss.svg" },
+      ],
+      keyFeatures: [
+        {
+          text: "Interactive map powered by MapBox",
+          icon: "/icons/mapbox.svg",
+        },
+        {
+          text: "E-commerce integration for online store",
+          icon: "/icons/ecommerce.svg",
+        },
+        {
+          text: "Fast, server-side rendering with Next.js",
+          icon: "/icons/server-side-rendering.svg",
+        },
+      ],
+      link: "https://novelle-farm-rm32n1yoj-julienlemee.vercel.app/",
+      github: "https://github.com/julienLeMee/yum-list",
+    },
+  ];
+
   return (
-    <div id="selected-projects" className="my-32 px-3 md:px-6 lg:px-10 flex flex-col">
-      <div className="w-full pb-16">
+    <div
+      id="selected-projects"
+      className="my-32 flex flex-col"
+    >
+      <div className="w-full px-3 md:px-6 lg:px-10 pb-16">
         <p className="max-w-[1920px] min-w-full pb-1 font-mono text-[13px] uppercase">
           [002. Selected Works]
         </p>
@@ -17,8 +129,8 @@ export default function Projects() {
         </p>
       </div>
 
-      <div className="w-full overflow-x-auto scrollbar-hide lg:pl-[calc(50vw-1000px)]">
-        <div className="flex w-max gap-x-3 py-5 md:gap-x-5">
+      <div className="">
+        <div className="flex py-5">
           {projects.map((project, index) => (
             <ProjectCard
               key={index}
