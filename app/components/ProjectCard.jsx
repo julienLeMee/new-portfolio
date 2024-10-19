c9ab52b04d33d61028e55aa8be90bf320a220b26
Julien Le Mee <julemee@gmail.com>
2024-10-18 20:56:29 -0400

    nouvelle cartes projects

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -12,7 +12,7 @@ export default function ProjectCard({
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="group h-[500px] inline-flex flex-1 hover:flex-[4] flex-col bg-[--color-dark-bg] transition-all duration-300 hover:bg-[--color-pink] dark:hover:bg-[--color-pink] relative"
+      className="group h-[500px] inline-flex flex-1 hover:flex-[4] flex-col bg-[--color-dark-bg] transition-all duration-300 hover:bg-[--color-pink] dark:hover:bg-[--color-pink] relative before:absolute before:inset-0 before:bg-gradient-to-t before:from-black before:via-transparent before:content-['']"
       style={{
         backgroundImage: `url(${imageSrc})`,
         backgroundSize: "cover",
@@ -20,14 +20,14 @@ export default function ProjectCard({
       }}
     >
       <div className="relative mb-4 h-full w-full overflow-hidden"></div>
-      <h3 className="mb-6 text-2xl leading-[1.1] md:text-[32px] text-white z-10 transition-all duration-500 ease-in-out custom-rotate uppercase text-[clamp(3rem,5vw,4.5rem)] font-extrabold absolute bottom-0 left-[50px]">
+      <h3 className="mb-6 text-2xl leading-[1.1] md:text-[32px] text-white z-10 transition-all duration-500 ease-in-out custom-rotate uppercase text-[clamp(3rem,5vw,4.5rem)] font-extrabold absolute bottom-0 left-[50px] drop-shadow-[2px_2px_4px_rgba(0,0,0,0.8)]">
         {title}
       </h3>
-      <div className="absolute top-8 right-8">
-        <p className="hidden group-hover:flex mb-6 flex-1 text-lg font-normal leading-[1.2] md:text-2xl md:leading-[1.1] text-white relative z-10 opacity-0 transition-opacity duration-300 delay-200 group-hover:opacity-100">
+      <div className="absolute w-3/4 bottom-0 right-0 p-8 text-right text-base opacity-0 transition-opacity duration-300 delay-200 group-hover:opacity-100">
+        {/* <p className="hidden text-right text-base group-hover:inline-flex justify-end mb-6 flex-1 font-normal leading-[1.2] md:leading-[1.1] text-white relative z-10 drop-shadow-[2px_2px_4px_rgba(0,0,0,0.8)]">
           {description}
-        </p>
-        <div className="hidden group-hover:flex flex-wrap gap-1 mb-3 relative z-10 opacity-0 transition-opacity duration-500 delay-200 group-hover:opacity-100">
+        </p> */}
+        <div className="hidden group-hover:inline-flex justify-end flex-wrap gap-1 mb-3 relative z-10">
           {technologies.map((tech, index) => (
             <ShinyBadge key={index} text={tech.name} logo={tech.logo} />
           ))}
