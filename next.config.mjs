eede9bebb2ed64da6f3db459755386257f329ac7
Julien Le Mee <julemee@gmail.com>
2024-10-23 18:03:14 -0400

    image logo prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['https://www.julienlemee.com/'],
+        domains: ['julienlemee.com'],
     },
 };
 
